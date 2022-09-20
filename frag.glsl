9d862ece800759eb01126aa384ee780d4ed6d331
Aditya Abhyankar <adityaa.edu@gmail.com>
2022-09-20 11:09:44 -0400

    Zoom in makes it pixelated still :(

--- frag.glsl
+++ frag.glsl
@@ -28,27 +28,22 @@ float zoom(float amt, float t) {
 
 void main()  {
     // Constants
-    float ratio = 1920.0 / 1080.0;
+    float ratio = 1920.0 / 1080.0;  // TODO: Adjust the ratio by the zoom, maybe what's happening is you're sampling values that are eventually too sparse in comparison with the zoom level!
 
     // Target coordinate
-    vec2 target = vec2(-0.5, 0.0);//vec2(-0.77568377, 0.13646737);
+    vec2 target = vec2(-0.77568377, 0.13646737);
 
     // Target min/max
     float amt = 2;
-    float zoomi = 4.0;
-    float zoomf = 4.0;//0.00001;
+    float zoomi = 0.00001;  // It's 2 - (-2) = 4.
+    float zoomf = 0.00001;
     float xlen = zoomi * pow(zoomf / zoomi, min(1, 20*time / 100.0));
     float ylen = xlen / ratio;
 
-    float xmin_target = target.x - (xlen/2);
-    float xmax_target = target.x + (xlen/2);
-    float ymin_target = target.y - (ylen/2);
-    float ymax_target = target.y + (ylen/2);
-
-    float xmin = xmin_target;//((-xlen/2) * (1 - pan(2, time))) + (time * xmin_target);
-    float xmax = xmax_target;//((xlen/2) * (1 - pan(2, time))) + (time * xmax_target);;
-    float ymin = ymin_target;//((-xlen/2) * (1 - pan(2, time))) + (time * xmin_target);
-    float ymax = ymax_target;//((xlen/2) * (1 - pan(2, time))) + (time * xmax_target);;
+    float xmin = target.x - (xlen/2);
+    float xmax = target.x + (xlen/2);
+    float ymin = target.y - (ylen/2);
+    float ymax = target.y + (ylen/2);
 
     vec2 mins = vec2(xmin, ymin);
     vec2 maxs = vec2(xmax, ymax);
@@ -56,7 +51,7 @@ void main()  {
     c = ((c + 1) / 2) * (maxs - mins) + mins;
 
     vec2 z0 = vec2(0.0, 0.0);
-    float t = stability(z0, c, int(30));
+    float t = stability(z0, c, int(300));
 
     // Color Mapping
     vec3 rgb = (vec3(0.3, 0.7, 0.8) * t) + ((1-t) * vec3(0.1, 0.2, 0.3));
