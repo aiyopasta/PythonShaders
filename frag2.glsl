32d747aedd89e5e70944e3b83ce85141d9c24dd1
Aditya Abhyankar <adityaa.edu@gmail.com>
2022-12-03 23:36:16 -0500

    OMG I SAVED ITJOSGJSLJDHG

--- frag2.glsl
+++ frag2.glsl
@@ -1,325 +1,50 @@
 #version 410
-#define PI 3.1415926535897932384626433832795028841971
-#define INF 100000.0
-
 uniform float time;  // elapsed time in seconds
 uniform sampler2D screenTexture;
 uniform int blah; // for testing stuff
-uniform int n;
-
 in vec3 v_color;
 in vec4 v_pos;
 in vec2 v_tex_coords;
 
 out vec4 frag_color;
-
-float rho = 50.0;  // dist from world origin to eye
-float theta = -PI / 2.0;
-float phi = PI / 2.0;
-float focus = 35.0;  // must be less than rho!
-float s_width = 10.0;  // screen width, in the imaginary world (not actual screen)
-
-// Helper functions
-float rng(vec2 co){
-    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
-}
-
-// Intersection Point
-struct IntersectionPoint {
-    float t;
-    vec3 pos;
-    vec3 nor;
-    bool on_light;  // true iff point lies on area-light
-};
-
-// Ray
-struct Ray {
-    vec3 origin;
-    vec3 dir;
-};
-
-
-// Primitive Shapes
-struct Sphere {
-    float radius;
-    vec3 center;
-};
-
-struct Triangle {
-    vec3 verts[3];  // Oriented counter-clockwise.
-    bool is_light;  // true iff it's an area-light
-};
-
-// Scene
-struct Scene {
-    int n_tris;
-    Triangle tris[20];
-    int n_spheres;
-    Sphere spheres[5];
-
-    // NOTE: INDEX 0 CANNOT BE A LIGHT!
-    int t_lights[5];  // list of indices in tris array corresponding to lights
-    int s_lights[5];  // list of indices in tris array corresponding to lights
-
-};
-
-
-// Intersect with triangle
-IntersectionPoint intersect_triangle(Triangle tri, Ray ray) {
-    // Triangle Data
-    vec3 v0 = tri.verts[0];
-    vec3 v1 = tri.verts[1];
-    vec3 v2 = tri.verts[2];
-    vec3 n = normalize(cross(v1 - v0, v2 - v0));
-
-    // Find intersection...
-    IntersectionPoint isect;
-    isect.t = INF;
-
-    // Triangle parallel with ray
-    if (dot(n, ray.dir) == 0.0) {
-        return isect;
-    }
-
-    float t = dot(n, v0 - ray.origin) / dot(n, ray.dir);
-    // Triangle behind the origin
-    if (t <= 0.0) {
-        return isect;
-    }
-
-    // In-out test
-    vec3 point = ray.origin + (t * ray.dir);
-    bool b0 = dot(cross(v1 - v0, point - v0), n) >= 0.0;
-    bool b1 = dot(cross(v2 - v1, point - v1), n) >= 0.0;
-    bool b2 = dot(cross(v0 - v2, point - v2), n) >= 0.0;
-    if ((b0 && b1) && b2) {
-        isect.t = t;
-        isect.pos = point;
-        isect.nor = n;
-    }
-    return isect;
-}
-
-// Intersect ray with sphere
-IntersectionPoint intersect_sphere(Sphere s, Ray ray) {
-    // Sphere data
-    vec3 center = s.center;
-    float radius = s.radius;
-
-    // Find intersection point...
-    IntersectionPoint point;
-    point.t = INF;
-
-    // Build quadratic
-    float A = dot(ray.dir, ray.dir);
-    float B = 2.0 * dot(ray.dir, ray.origin - center);
-    float C = dot(ray.origin - center, ray.origin - center) - (radius * radius);
-    float disc = (B * B) - (4.0 * A * C);
-
-    // Solve quadratic
-    if (disc < 0.0) {
-        return point;
-    }
-    float t = min((-B + sqrt(disc)) / (2.0 * A), (-B - sqrt(disc)) / (2.0 * A));
-    if (t >= 0.0) {
-        point.t = t;
-        point.pos = ray.origin + (t * ray.dir);
-        point.nor = normalize(point.pos - center);
-    }
-    return point;
-}
-
-
-// Create Scene 2 — Box with Area Light + 2 Spheres
-Scene scene2() {
-    Scene scene;
-
-    // Box parameters. It opens up on the negative y-axis. 'h' below stands for 'half'.
-    float hx = 30.0 / 2.0;
-    float hy = 30.0 / 2.0;
-    float hz = 15.0 / 2.0;
-
-
-    // Box will be a collection of triangles.
-    const int n_tris = 1;//(5 + 1) * 2;
-    scene.n_tris = n_tris;
-    const float p = 0.3;  // percentage smaller area-light is compared to ceiling.
-    const float dz = -0.1; // z-displacement from ceiling to area light
-    Triangle tris[n_tris] = Triangle[n_tris] (
-        // Floor
-        Triangle(vec3[3]( vec3(-hx, -hy, -hz), vec3(hx, -hy, -hz), vec3(-hx, hy, -hz) ), false)
-//        Triangle(vec3[3]( vec3(hx, hy, -hz), vec3(-hx, hy, -hz), vec3(hx, -hy, -hz) ), false),
-//        // Back Wall
-//        Triangle(vec3[3]( vec3(-hx, hy, -hz), vec3(hx, hy, -hz), vec3(-hx, hy, hz) ), false),
-//        Triangle(vec3[3]( vec3(hx, hy, hz), vec3(-hx, hy, hz), vec3(hx, hy, -hz) ), false),
-//        // Left Wall
-//        Triangle(vec3[3]( vec3(-hx, -hy, -hz), vec3(-hx, hy, -hz), vec3(-hx, -hy, hz) ), false),
-//        Triangle(vec3[3]( vec3(-hx, hy, hz), vec3(-hx, -hy, hz), vec3(-hx, hy, -hz) ), false),
-//        // Right Wall
-//        Triangle(vec3[3]( vec3(hx, -hy, -hz), vec3(hx, hy, -hz), vec3(hx, -hy, hz) ), false),
-//        Triangle(vec3[3]( vec3(hx, hy, hz), vec3(hx, -hy, hz), vec3(hx, hy, -hz) ), false),
-//        // Ceiling
-//        Triangle(vec3[3]( vec3(-hx, -hy, hz), vec3(hx, -hy, hz), vec3(-hx, hy, hz) ), false),
-//        Triangle(vec3[3]( vec3(hx, hy, hz), vec3(-hx, hy, hz), vec3(hx, -hy, hz) ), false),
-//
-//        // Area Light
-//        Triangle(vec3[3]( vec3(-p*hx, -p*hy, hz+dz), vec3(p*hx, -p*hy, hz+dz), vec3(-p*hx, p*hy, hz+dz) ), true),
-//        Triangle(vec3[3]( vec3(p*hx, p*hy, hz+dz), vec3(-p*hx, p*hy, hz+dz), vec3(p*hx, -p*hy, hz+dz) ), true)
-
-    );
-    for (int i=0; i<n_tris; ++i) {
-        scene.tris[i] = tris[i];
-    }
-
-    // Throw in a couple of spheres too
-    const float radius = 4.0;
-//    const int n_spheres = 2;
-//    scene.n_spheres = n_spheres;
-//    Sphere spheres[n_spheres] = Sphere[n_spheres](
-//        Sphere(radius, vec3(0.4 * hx, -0.2 * hy, -hz + radius)),
-//        Sphere(radius, vec3(-0.4 * hx, 0.5 * hy, -hz + radius))
-//    );
-//    for (int i=0; i<n_spheres; ++i) {
-//        scene.spheres[i] = spheres[i];
-//    }
-
-    // Indicate to the scene which primitives are lights (only 2 triangles in this case)
-    scene.t_lights[0] = n_tris-1;
-    scene.t_lights[1] = n_tris-2;
-
-    return scene;
-}
-
-
-// Generic intersection method of ray with scene
-IntersectionPoint intersect_scene(Scene scene, Ray ray) {
-    IntersectionPoint minpoint;
-    minpoint.t = INF;
-
-    // Triangle intersection
-    for (int i=0; i<scene.n_tris; ++i) {
-        IntersectionPoint isect = intersect_triangle(scene.tris[i], ray);
-        if (isect.t < minpoint.t) {
-            minpoint = isect;
-            minpoint.on_light = scene.tris[i].is_light;
-        }
-    }
-
-    // Sphere intersection
-    for (int i=0; i<scene.n_spheres; ++i) {
-    IntersectionPoint isect = intersect_sphere(scene.spheres[i], ray);
-        if (isect.t < minpoint.t) {
-            minpoint = isect;
-            minpoint.on_light = false;
-        }
-    }
-
-
-    return minpoint;
-}
-
 void main() {
-    vec4 tex = texture(screenTexture, v_tex_coords);
-    vec3 rgb;
-    float n_ = float(n);
-    if (blah > 0) {
-//        if (v_pos.x < -0.5) {
-//            frag_color = vec4(1.0);
-//        }
-//        else {
-//            frag_color = vec4(0.0);
-//        }
 
-        if ((v_pos.x < -0.5) && (n == 1)) {
-            rgb = vec3(1.0, 0.0, 0.0);
+    if (blah == 1.0) {
+//        frag_color = vec4(v_color, 1.0);
+        if (v_pos.x > 0 && v_pos.y < 0) {
+            frag_color = vec4(1.0, 0.0, 0.0, 1.0);
         }
         else {
-            rgb = tex.rgb;
+            frag_color = vec4(0.0);
         }
-
-//        if (n % 3 == 0) {
-//            if (v_pos.x < -0.5) {
-//                rgb = vec3(1.0, 0.0, 0.0);
-//            }
-//        }
-//        else {
-//            if (v_pos.x < -0.5) {
-//                rgb = vec3(0.0, 0.0, 0.0);
-//            }
-//        }
-
-        frag_color = vec4(rgb, 1.0);//vec4((rgb + ((n_-1.0) * tex.rgb)) / n_, 1.0);
-
-        // Dynamic constants
-//        vec2 uv = ((v_pos.xy / 2.0) + 1.0) / gl_FragCoord.xy;
-//        vec3 rgb;
-//        if ((uv.x > 0.5) || (uv.y > 0.5)) {
-//            rgb = vec3(1.0, 0.0, 0.0);
-//        }
-//
-//        // Output to screen.
-//        frag_color = vec4(rgb,1.0);
-
-//        float AR = gl_FragCoord.x / gl_FragCoord.y;
-//        float inv_AR = 1.0 / AR;
-//
-//        // Get camera pos (in range [-1,1]^2) from mouse
-////        vec2 mouseUV = ((iMouse.xy / iResolution.xy) * 2.0) - vec2(1.0);
-////        theta -= mouseUV.x * 2.0;
-////        phi += mouseUV.y * 2.0;
-//
-//        // Point light (the actual light for Blinn-Phong in this demo)
-//        vec3 light_pos = 20.0 * vec3(0.0, -1.0, 0.0);
-//        vec3 light_col = vec3(1.0, 0.0, 0.0);
-//        vec3 ambient_col = vec3(1.0, 0.0, 0.0);
-//
-//        // Set eye position and virtual screen dimensions
-//        vec3 eye = rho * vec3(sin(phi) * cos(theta), sin(phi) * sin(theta), cos(phi));
-//        float s_height = s_width * inv_AR;
-//
-//        // Turn uv from [0,1]^2 range to [-1,1]^2 range (with y still up)
-//        vec2 uv_prime = (uv * 2.0) - vec2(1.0);
-//
-//        // Spherical unit vectors
-//        vec3 theta_hat = vec3(-sin(theta), cos(theta), 0);
-//        vec3 phi_hat = -vec3(cos(phi) * cos(theta), cos(phi) * sin(theta), -sin(phi));
-//        vec3 point = (eye * (focus / rho)) + ((uv_prime.x * (s_width / 2.0) * theta_hat) + (uv_prime.y * (s_height / 2.0) * phi_hat));
-//
-//        // Scene to use
-//        Scene s = scene2();
-//
-//        // Single ray (per pixel)
-//        Ray r;
-//        r.dir = normalize(point - eye);
-//        r.origin = eye;
-//
-//        IntersectionPoint isect = intersect_scene(s, r);
-//        vec3 rgb;
-//        if (isect.on_light) {rgb = vec3(1.0);}
-//        else if (isect.t != INF) {
-//            vec3 l = normalize(light_pos - isect.pos);
-//            vec3 v = normalize(-r.dir);
-//            vec3 h = normalize(l + v);
-//            vec3 n = isect.nor;
-//
-//            float ambient = 0.6;
-//            float diffuse = max(dot(n, l), 0.0);
-//            float specular = diffuse != 0.0 ? pow(max(dot(n, h), 0.0), 50.0) : diffuse;
-//            rgb = vec3((0.3 * ambient_col * ambient) + light_col * ((0.6 * diffuse) + (0.3 * specular)));
-//        }
-//
-//        // Crude test to see if it's averaging.
-//    //    if (iFrame % 2 == 0) {
-//    //        rgb = vec3(rng(vec2(iFrame, iFrame)));
-//    //    }
-//
-//        // Store possible mouse click
-////        bool moved = (iMouse / iResolution.x).z > 0.0;
-//        frag_color = vec4(rgb, 1.0);
+    } else if (blah == 2.0) {
+        if (v_pos.x < 0) {
+            frag_color = vec4(0.0, 1.0, 0.0, 1.0);
+        }
+        else {
+            frag_color = vec4(0.0);
+        }
     }
 
-    // If it's the default frame buffer
     else {
-        frag_color = tex;
+
+//        frag_color = texture(screenTexture, vec2(0.0, ));
+
+
+        frag_color = texture(screenTexture, v_tex_coords);
     }
+
+
+
+//    vec2 tex_coords = gl_FragCoord.xy / vec2(1920.0, 1080.0);
+////    frag_color = vec4(0.0023)
+//
+//    vec3 rgb;
+//    if (tex_coords.x < 1.0) {
+//        rgb = vec3(0.001, 0.0, 0.0);
+//    }
+//    else {
+//        rgb = vec3(0.0, 0.001, 0.0);
+//    }
+//    frag_color = vec4(rgb, 1.0) + texture(prev_col, tex_coords);
 }
--- pingpong.py
+++ pingpong.py
@@ -7,7 +7,6 @@ from PIL import Image
 from collections import namedtuple
 from ctypes import c_float, c_void_p, cast
 
-
 def readall(filename:str):
     with open(filename) as f:
         lines = f.read()  # reads entire file as 1 string
@@ -48,7 +47,7 @@ glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
 glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 1)
 glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
 glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-window = glfw.create_window(1920, 1080, "Hello OpenGL!", None, None)
+window = glfw.create_window(1728, 1051, "Hello OpenGL!", None, None)
 if not window:
     glfw.terminate()
     raise Exception("glfw window cannot be created!")
@@ -68,10 +67,15 @@ glBindVertexArray(VAO)
 
 # Note: Each entry is a 32-bit float, so 32/8 = 4 bytes total.
 # Since each vertex's data has 8 elements (3 coordinates + 3 RGB + 2 tex coords) in total each vertex data is 8*4 = 32 bytes.
-vertex_data = [-1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-                1.0, -1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
-               -1.0,  1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0,
-                1.0,  1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+vertex_data = [-1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                -1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+                1.0,  1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0]
+
+# vertex_data = [-1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+#                 1.0, -1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+#                -1.0,  1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0,
+#                 1.0,  1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 
 vertex_data = np.array(vertex_data, dtype=np.float32)
 
@@ -100,8 +104,6 @@ time_idx = glGetUniformLocation(shader, "time")
 screenTexture_idx = glGetUniformLocation(shader, "screenTexture")
 # 5. Send the uniform blah variable.
 blah_idx = glGetUniformLocation(shader, "blah")
-# 6. Send the uniform n variable.
-n_idx = glGetUniformLocation(shader, "n")
 
 
 # Create and bind FBO1
@@ -113,7 +115,7 @@ color_attachment1 = glGenTextures(1)
 
 # Bind the texture for use as "GL_TEXTURE_2D"
 glBindTexture(GL_TEXTURE_2D, color_attachment1)
-glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB, GL_UNSIGNED_BYTE, None)
+glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1728 * 2, 1051 * 2, 0, GL_RGB, GL_UNSIGNED_BYTE, None)
 
 # Set its parameters
 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
@@ -137,7 +139,7 @@ color_attachment2 = glGenTextures(1)
 
 # Bind the texture for use as "GL_TEXTURE_2D"
 glBindTexture(GL_TEXTURE_2D, color_attachment2)
-glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB, GL_UNSIGNED_BYTE, None)
+glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1728 * 2, 1051 * 2, 0, GL_RGB, GL_UNSIGNED_BYTE, None)
 
 # Set the texture parameters
 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
@@ -152,132 +154,43 @@ glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colo
 if glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE:
     raise Exception("Error setting up FBO2!")
 
-
 # Tells OpenGL to use our shader program.
 glUseProgram(shader)
 glClearColor(0, 0.1, 0.1, 1)  # Sets background color
 
-# TODO: Now, every time the loop runs, what we wanna do is:
-# 1. Send the correct blah variable, based on value of ping_pong.
-# 2. Activate, bind, and send the texture of the previous draw into the fragment shader.
-# 3. Bind the correct framebuffer, and clear it.
-# 4. Draw onto that framebuffer.
-# 5. Do everything you did in the while loop for the epilepsy example.
+# 1. Bind the first framebuffer, and clear it.
+glBindFramebuffer(GL_FRAMEBUFFER, FBO1); glClear(GL_COLOR_BUFFER_BIT)
+# 2. Update the blah_idx variable with the value 0.
+glUniform1i(blah_idx, 1)
+# 2. Draw on the first framebuffer.
 
-ping_pong = True
-n = 1
-start = time.time()
+# glViewport(0, 0, 1728, 1051)
+print(glfw.get_window_size(window))
+
+glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+# 3. Bind the second framebuffer.
+glBindFramebuffer(GL_FRAMEBUFFER, FBO2)
+# 4. Send the value 1 to the blah_idx variable.
+glUniform1i(blah_idx, 2)
+# 5. Draw on the second framebuffer.
+glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+# 6. In the while loop: i) If ping_pong, Bind the first texture and draw it. Otherwise, do it for the second. ii) ping_pong = not ping_pong.
+# We should see an oscillation between what we've drawn in both of the two FBOs.
+
 while not glfw.window_should_close(window):
-    print(n)
-    # 1.
-    glUniform1i(blah_idx, 1 if ping_pong else 2)
-    glUniform1i(n_idx, n)
-    glUniform1f(time_idx, time.time() - start)
-    # 2.
     glActiveTexture(GL_TEXTURE0)
-    glBindTexture(GL_TEXTURE_2D, color_attachment1 if not ping_pong else color_attachment2)
-    glUniform1i(screenTexture_idx, 0)
-    # 3.
-    glBindFramebuffer(GL_FRAMEBUFFER, FBO1 if ping_pong else FBO2)
-    glClear(GL_COLOR_BUFFER_BIT)
-    # 4.
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
-    # 5.
-    glActiveTexture(GL_TEXTURE0)
-    glBindTexture(GL_TEXTURE_2D, color_attachment1 if ping_pong else color_attachment2)   # redundant?
-    glUniform1i(screenTexture_idx, 0)
-    # Let the fragment shader know to run the code for simply displaying the texture onto the screen.
+    glBindTexture(GL_TEXTURE_2D, color_attachment1)   # flip the color attachment to view the different FBOs!
     glUniform1i(blah_idx, -2394)
-    # Bind the default FBO, clear it, and draw on it using our fragment shader.
+    glUniform1i(screenTexture_idx, 0)
+
     glBindFramebuffer(GL_FRAMEBUFFER, 0)
     glDisable(GL_DEPTH_TEST)
     glClear(GL_COLOR_BUFFER_BIT)
+    # glViewport(0, 0, 1920, 1080)
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
 
-    # Toggle ping pong, increment n
-    ping_pong = not ping_pong
-    n += 1
-
-    # The regular stuff
     glfw.swap_buffers(window)
     glfw.poll_events()
-    time.sleep(1)
-
-
-# # EPILEPSY ————
-# # 1. Bind the first framebuffer, and clear it.
-# glBindFramebuffer(GL_FRAMEBUFFER, FBO1); glClear(GL_COLOR_BUFFER_BIT)
-# # 2. Update the blah_idx variable with the value 0.
-# glUniform1i(blah_idx, 1)
-# # 2. Draw on the first framebuffer.
-# glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
-# # 3. Bind the second framebuffer.
-# glBindFramebuffer(GL_FRAMEBUFFER, FBO2)
-# # 4. Send the value 1 to the blah_idx variable.
-# glUniform1i(blah_idx, 2)
-# # 5. Draw on the second framebuffer.
-# glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
-# # 6. In the while loop: i) If ping_pong, Bind the first texture and draw it. Otherwise, do it for the second. ii) ping_pong = not ping_pong.
-# # We should see an oscillation between what we've drawn in both of the two FBOs.
-#
-# ping_pong = True
-# while not glfw.window_should_close(window):
-#     # Activate, bind, and send the correct texture into the fragment shader.
-#     glActiveTexture(GL_TEXTURE0)
-#     glBindTexture(GL_TEXTURE_2D, color_attachment1 if ping_pong else color_attachment2)   # flip the color attachment to view the different FBOs!
-#     glUniform1i(screenTexture_idx, 0)
-#
-#     # Let the fragment shader know to run the code for simply displaying the texture onto the screen.
-#     glUniform1i(blah_idx, -2394)
-#
-#     # Bind the default FBO, clear it, and draw on it using our fragment shader.
-#     glBindFramebuffer(GL_FRAMEBUFFER, 0)
-#     glDisable(GL_DEPTH_TEST)
-#     glClear(GL_COLOR_BUFFER_BIT)
-#     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
-#
-#     # Toggle ping pong
-#     ping_pong = not ping_pong
-#
-#     # Regular stuff
-#     glfw.swap_buffers(window)
-#     glfw.poll_events()
-
-
-
-
-
-
-
-
-# ping_pong = True
-# start = time.time()
-# while not glfw.window_should_close(window):
-#     # Choice: True = 1, False = 2
-#     # Steps:
-#     # 1. Bind the previous FBO's texture (redundant, but that's okay).
-#     glBindTexture(GL_TEXTURE_2D, color_attachment1 if ping_pong else color_attachment2)
-#     # 2. ping_pong = not ping_pong.
-#     ping_pong = not ping_pong
-#     # 3. Bind the new FBO corresponding to the (new) ping_pong. CLEAR IT.
-#     glBindFramebuffer(GL_FRAMEBUFFER, FBO1 if ping_pong else FBO2)
-#     glClear(GL_COLOR_BUFFER_BIT)
-#     # 4. Send the previous texture to the sampler2D of this FBO. (Using glUniformi)
-#     glUniform1i(previousColor_idx, 0)  # the currently bound texture is sent
-#     # 5. Draw onto this FBO (using the sampler2D object in the fragment shader).
-#     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
-#     # 6. Bind the default FBO again. Clear it.
-#     glBindFramebuffer(GL_FRAMEBUFFER, 0)
-#     glClear(GL_COLOR_BUFFER_BIT)
-#     # 7. Bind the texture of the FBO we drew on.
-#     glBindTexture(GL_TEXTURE_2D, color_attachment1 if ping_pong else color_attachment2)
-#     # 8. Use the bound texture to draw the FBO's data onto the default FBO.
-#     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
-#
-#
-#     # Swap the buffers and poll for events
-#     glfw.swap_buffers(window)
-#     glfw.poll_events()
 
 # Clean up
 glfw.terminate()
